aa123e47ca6555b604b2c8393efb1b2d241972bd
mrkindy <admin@mrkindy.com>
2024-12-03 09:29:13 +0200

    Initiate Hijri Date Package

---
+++ examples.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Kindy\HijriDate\Hijirdatetime;
+use Kindy\HijriDate\Calendar;
+
+//echo new Hijirdatetime();
+
+//echo (new Hijirdatetime())->format('D _j _M _Yهـ (j-m-Yم)');
+
+//echo (new Hijirdatetime("2024-12-3"))->format('D _j _M _Yهـ (j-m-Yم)');
+
+//$d = Hijirdatetime::createFromHijri(1446,6,2);
+//echo $d->format('D _j _M _Yهـ (j-m-Yم)');
+
+//$date = new Hijirdatetime();
+//echo $date;
+//$date->setDateHijri(1445,9,1);
+//echo $date;
+
+
+//$calendar = new Calendar([
+//     'umalqura'=> TRUE,
+//     'adj_data' => '1426 => 57250, 1429 => 57339,'
+// ]);
+// echo $calendar->JDToHijri(2450000);
